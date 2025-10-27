8169cd008f3f01f733782e302ec7f1af1c62078b
EnderEclipz <73099064+EnderEclipz@users.noreply.github.com>
2025-10-27 15:44:45 +1300

    1.0.0

---
+++ script.js
@@ -0,0 +1,4 @@
+// Simple interactivity example
+document.getElementById('learn-more').addEventListener('click', function() {
+  alert('Thanks for your interest! This is a basic starter site you can build on.');
+});
